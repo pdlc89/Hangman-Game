0aa81ed2ed6c0e6f87e385558dd51ce1edb6c355
Pedro <pcruz9332@gmail.com>
2017-10-17 21:28:16 -0700

    closer

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,10 +13,19 @@ document.onkeyup = function() {
 
 
 var words = ["HELLO", "WORLD", "SCHOOL", "COMPUTER"];
+var 
 
 
 var computerGuess = words[Math.floor(Math.random()*words.length)];
 	console.log (computerGuess);
 
-var letters
+var holder = computerGuess.split("");
+	console.log(holder);
+
+var putter = document.getElementById('1');
+
+putter.innerHTML = holder[0];
+
+if computerGuess = words[0];
+	if document.
 }
