b39d1ef19376b2904c66a8632950166f8e954d09
PEDRO DE LA CRUZ MARTINEZ <pcruz9332@gmail.com>
2017-10-18 23:15:36 -0700

    almost-there

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,31 +1,35 @@
+$(document).ready(function() {
 
 
 
 
+	document.getElementById("start-button").addEventListener("click", function(){
+		var words = ["HELLO", "WORLD", "SCHOOL", "COMPUTER"];
 
+		var computerGuess = words[Math.floor(Math.random()*words.length)];
+		console.log (computerGuess);
 
-document.onkeyup = function() {
-	var userguess = String.fromCharCode(event.keyCode).
-		toUpperCase();
-
-	console.log (userguess);
-
-
-
-var words = ["HELLO", "WORLD", "SCHOOL", "COMPUTER"];
-var 
-
-
-var computerGuess = words[Math.floor(Math.random()*words.length)];
-	console.log (computerGuess);
-
-var holder = computerGuess.split("");
-	console.log(holder);
-
-var putter = document.getElementById('1');
-
-putter.innerHTML = holder[0];
-
-if computerGuess = words[0];
-	if document.
-}
+		var holder = computerGuess.split("");
+		console.log(holder);
+		
+//Words for computer to choose from
+		document.onkeyup = function() {
+		var userGuess = String.fromCharCode(event.keyCode).
+			toUpperCase();
+			console.log (userGuess);
+				wrapper = document.getElementById('tower');
+				span = wrapper.getElementsByTagName('span');
+				tags = [];
+					for (i = 0; i < span.length; i++) {
+    					tags[i] = span[i].getElementsByTagName('u');
+					}
+			for (var i = 0; i < holder.length; i++) {
+				if (holder[i].indexOf(userGuess) != -1){
+					var putter = document.getElementById(i);
+					putter.innerHTML = holder[i];
+				}
+			}
+		
+		}
+	});
+});
