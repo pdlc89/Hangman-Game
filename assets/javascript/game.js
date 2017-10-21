5f04ac96fa985820edfbcae1cf0a920aa4674f7c
PEDRO DE LA CRUZ MARTINEZ <pcruz9332@gmail.com>
2017-10-21 09:01:56 -0700

    not-complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,10 +7,11 @@ $(document).ready(function() {
 				var tags = [];
 //The loop that will place the correct id tag from the document to the Array 
 			for (i = 0; i < span.length; i++) {
-    			tags[i] = span[i].getElementsByTagName('u');
+    			tags[i] = span[i].getElementsByTagName('p');
 					}
-
-
+//creates an array of content in the doc
+			var content = $('u').html();
+			console.log(content);
 	document.getElementById("start-button").addEventListener("click", function(){
 		var words = ["HOME", "WORLD", "STORE", "COMPUTER", "COMPLY", "ANTONYMS"];
 
@@ -20,57 +21,24 @@ $(document).ready(function() {
 		var holder = computerGuess.split("");
 //shoows the computer's guess on the console
 		console.log(holder);
-//The loop that will place the correct guess to the corresponding spot in the document
-
-				for (var i = 0; i < holder.length; i++) {
-					if (holder[i].indexOf(value1) != -1){
-//creates a variable that gets the elements from the document
-						var valuePutter = document.getElementById(i);
-//Puts the users correct guess to display on to the document
-						valuePutter.innerHTML = holder[i];
-//creates a variable of the users correct						
-					}
-	 			}
-
-
-//keeps track of correct guesses
-/*for (var j = 0; j < holder.length; j++) {
-			if (holder[j].indexOf(counter)
-		}*/
 //Words for computer to choose from
 		document.onkeyup = function() {
 		var userGuess = String.fromCharCode(event.keyCode).
 //Takes the user's guess and converts it to upper case charactors
 			toUpperCase();
 //Logs the guesses to console
-			console.log (userGuess);
+			//console.log (userGuess);
 			var userError = 0;
-			var value1 = 0;
-
-			
 //The loop that will place the correct guess to the corresponding spot in the document
 				for (var i = 0; i < holder.length; i++) {
 					if (holder[i].indexOf(userGuess) != -1){
 //creates a variable that gets the elements from the document
 						var putter = document.getElementById(i);
 //Puts the users correct guess to display on to the document
-						putter.innerHTML = holder[i];
-							var value2 = 0;
-							for (var i = 0; i < holder.length; i++) {
-								if (holder[i].indexOf(value1) != -1) {
-//creates a variable that gets the elements from the document
-									var valuePutter = document.getElementById(i);
-//Puts the users correct guess to display on to the document
-									valuePutter.innerHTML = holder[i];
-//creates a variable of the users correct						
-								}
-								else {
-									 value2 = 2;
-								}
-	 						}						
+						putter.innerHTML = holder[i];						
 					}
 					else {
-						userError = true;
+						userError++;
 					}
 
 	 			}	   
